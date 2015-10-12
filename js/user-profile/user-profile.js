d02a81da670482d5a75971a93c1ef751fc24f768
mahyargigi <mahyarghoraian@gmail.com>
2015-10-12 14:16:11 +0330

    search page wishlist tabcontent added

--- js/user-profile/user-profile.js
+++ js/user-profile/user-profile.js
@@ -1,7 +1,12 @@
 /**
  * Created by User on 10/5/15.
  */
-$('.remove-btn').on('click',function(){
+$(document).ready((function(){
+    $('.close').on('click',function(){
+        $(this).closest('.row-li').remove();
+    });
+    $('.remove-btn').on('click',function(){
     var element = $(this).closest('.travel-info');
     element.remove();
 });
+}));

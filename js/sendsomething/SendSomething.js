ca091f0bb5c5bdcbd6a544b9bdf8f6a3ea3d8fb1
mahyargigi <mahyarghoraian@gmail.com>
2015-11-18 01:13:21 +0330

    send something page upload picture bug fixed+ to,from labels were added + vertical line added+ some other bugs repairment

---
+++ js/sendsomething/SendSomething.js
@@ -0,0 +1,110 @@
+/**
+ * Created by User on 8/25/15.
+ */
+$(document).ready(function(){
+    $('#toDate').bootstrapMaterialDatePicker({ weekStart : 0 ,time: false } );
+    $('#fromDate').bootstrapMaterialDatePicker({ weekStart : 0 ,time: false } );
+//    var slider = document.getElementById('slider');
+//    noUiSlider.create(slider, {
+//        start: [20, 80],
+//        connect: true,
+//        range: {
+//            'min': 0,
+//            'max': 100
+//        }
+//    });
+//    var connectSlider = document.getElementById('slider');
+//
+//    noUiSlider.create(connectSlider, {
+//        start: 40,
+//        connect: 'upper',
+//        range: {
+//          'min': 0,
+//          'max': 100
+//        }
+//    });
+    $('#slider').on('change',function(){
+        console.log((this).value);
+        $('#slider-value').html((this).value+"$");
+    });
+    var input1 = document.getElementById('dest-input');
+    input1.onkeypress = function(){
+        $('#dest-input').geocomplete();
+    }
+    var input2 = document.getElementById('source-input');
+    input2.onkeypress = function(){
+        $('#source-input').geocomplete();
+    }
+    $('#slider-value').html($('#slider').val()+"$");
+
+    $('.upload-image1').on('click',function(){
+        if($('#user-image1').attr('uploaded')==='false'){
+            document.getElementById("fileInput1").click();
+        }
+                console.log("1 clicked!");
+        return false ;
+    });
+    $('.upload-image-link1').hover(function(){
+        if($('#user-image1').attr('uploaded')==='true'){
+            $('.top-image1').css('visibility','visible');
+            $('#user-image1').css('opacity','0.3');
+            $(this).on('click',function(){
+            $('.top-image1').css('visibility','hidden');
+            $('#user-image1').attr('src','img/sendsomething/dotted1.png');
+            $('#user-image1').attr('uploaded','false');
+            return false ;
+            });
+        }
+    },function(){
+        $('.top-image1').css('visibility','hidden');
+        $('#user-image1').css('opacity','1');
+    });
+
+    $('.upload-image2').on('click',function(){
+        if($('#user-image2').attr('uploaded')==='false'){
+            document.getElementById("fileInput2").click();
+        }
+                console.log("2 clicked!");
+        return false ;
+    });
+    $('.upload-image-link2').hover(function(){
+        if($('#user-image2').attr('uploaded')==='true'){
+            $('.top-image2').css('visibility','visible');
+            $('#user-image2').css('opacity','0.3');
+            $(this).on('click',function(){
+            $('.top-image2').css('visibility','hidden');
+            $('#user-image2').attr('src','img/sendsomething/dotted1.png');
+            $('#user-image2').attr('uploaded','false');
+            return false ;
+            });
+        }
+    },function(){
+        $('.top-image2').css('visibility','hidden');
+        $('#user-image2').css('opacity','1');
+    });
+
+    $('.upload-image3').on('click',function(){
+        if($('#user-image3').attr('uploaded')==='false'){
+            document.getElementById("fileInput3").click();
+        }
+        console.log("3 clicked!");
+        return false ;
+    });
+    $('.upload-image-link3').hover(function(){
+        if($('#user-image3').attr('uploaded')==='true'){
+            $('.top-image3').css('visibility','visible');
+            $('#user-image3').css('opacity','0.3');
+            $(this).on('click',function(){
+            $('.top-image3').css('visibility','hidden');
+            $('#user-image3').attr('src','img/sendsomething/dotted1.png');
+            $('#user-image3').attr('uploaded','false');
+            return false ;
+            });
+        }
+    },function(){
+        $('.top-image3').css('visibility','hidden');
+        $('#user-image3').css('opacity','1');
+    });
+
+
+});

c20ecb5403653b5ea38e59e3efe75d22d5147c81
mahyargigi <mahyarghoraian@gmail.com>
2016-08-10 13:00:44 +0430

    add newest order page

--- js/global/english-input.js
+++ js/global/english-input.js
@@ -2,11 +2,16 @@ $("input").on("keypress", function(event) {
     //console.log("typing");
     var englishAlphabetAndWhiteSpace = /[A-Za-z ]/g;
     var key = String.fromCharCode(event.which);
-    if (englishAlphabetAndWhiteSpace.test(key) || key.match(/^\d+$/) || (key==='@') ||(key==='.') || (key==='_')) {
+    if($(this).hasClass('url')){
         return true;
     }
     else{
-        return false;
+        if (englishAlphabetAndWhiteSpace.test(key) || key.match(/^\d+$/) || (key==='@') ||(key==='.') || (key==='_')) {
+        return true;
+        }
+        else{
+            return false;
+        }
     }
 });
 $("input").on("paste", function(event) {

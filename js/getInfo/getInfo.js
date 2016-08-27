a9cfc7999a6d4fa925f2b6903dd81c181ff77a1f
mahyargigi <mahyarghoraian@gmail.com>
2016-08-27 11:52:06 +0430

    get info geocompleting test

---
+++ js/getInfo/getInfo.js
@@ -0,0 +1,67 @@
+/**
+ * Created by User on 8/24/2016.
+ */
+$(document).ready(function(){
+   $('.prof-pic').on('click',function(){
+        if($('#prof-pic').attr('uploaded')==='false'){
+            document.getElementById("fileInput").click();
+        }
+        return false ;
+    });
+    $('.upload-image-link').hover(function(){
+        if($('#prof-pic').attr('uploaded')==='true'){
+            $('.top-image').css('visibility','visible');
+            $('#prof-pic').css('opacity','0.3');
+            $(this).on('click',function(){
+            $('.top-image').css('visibility','hidden');
+            $('#prof-pic').attr('src','img/new-order/dotted1.png');
+            $('#prof-pic').attr('uploaded','false');
+            $('.forget-it').html('!بی خیال');
+            return false ;
+            });
+        }
+    },function(){
+        $('.top-image').css('visibility','hidden');
+        $('#prof-pic').css('opacity','1');
+    });
+    $('.forget-it').on('click',function(){
+       $('.prof-pic-div').fadeOut(500);
+       $('.job-div').delay(500).fadeIn(100);
+    });
+    $('.prev-phase').on('click',function(){
+       $('.job-div').fadeOut(500);
+       $('.prof-pic-div').delay(500).fadeIn(100);
+    });
+    $('.forget-it2').on('click',function(){
+       $('.job-div').fadeOut(500);
+       $('.city-div').delay(500).fadeIn(100);
+    });
+    $('.job-input').on('keyup' , function(){
+       if($(this).val() !== ''){
+           $('.forget-it2').html('مرحله بعد');
+       }else{
+           $('.forget-it2').html('بی خیال!');
+       }
+    });
+    $('.city-input').on('keyup' , function(){
+       if($(this).val() !== ''){
+           $('.forget-it3').html('مرحله بعد');
+       }else{
+           $('.forget-it3').html('بی خیال!');
+       }
+    });
+            console.log("is ok!!");
+            var acOptions = {
+            types: ['(cities)']
+            };
+            $('#city-input').geocomplete(acOptions).bind("geocode:result", function(event, click){
+                var city = click.address_components[0].long_name;
+                console.log("geo completeing!");
+            });
+            $('#city-input').change(function(){
+               console.log("yoohooo");
+            });
+            $('#city-input').on('keyup',function(){
+               console.log("a simple log");
+            });
+});
